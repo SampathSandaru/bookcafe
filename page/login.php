33ced323a3293f7410722d3dc35d80acad2421b9
SampathSandaru <lahirusampath8899@gmail.com>
2020-01-10 18:42:00 +0530

    login page

--- page/login.php
+++ page/login.php
@@ -10,7 +10,14 @@
     
      <link href="../css/login_page.css"  rel="stylesheet" type="text/css">
     
+    <script src="../css/jquery-3.4.1.js"></script>
+    
     <style>
+        .btn_l{border: none;
+            background-color:#edf7f3;
+            width: 49%;
+            height: 40px;
+        }
         
     </style>
     
@@ -44,8 +51,8 @@
     
     <div class="div2">
         
-        <form>
-            <table class="table_1">
+        <form method="post">
+            <table class="table_1" id="log_tb">
                 <tr>
                     <td>User Name</td>
                 </tr>
@@ -56,7 +63,7 @@
                     <td>Password</td>
                 </tr>
                 <tr>
-                    <td><input type="password" name="email"  class="form-control" placeholder="password">  </td>
+                    <td><input type="password" name="password"  class="form-control" placeholder="password">  </td>
                 </tr>
                 <tr>
                     <td>
@@ -65,13 +72,69 @@
                     </td>
                 </tr>
                 <tr>
-                    <td>Forgety Your Password?</td>
+                    <td>Forget Your Password?</td>
+                </tr>
+                 <tr>
+                    <td> Don't have an account! <a href="#" id="sing_in">Sign Up Here</a></td>
                 </tr>
             </table>
         </form>
-        
+        <!--user register form-->
+        <form method="post">
+            <table class="table2" id="rge_tb">
+                <tr>
+                   <td>First Name</td>
+                   <td><input type="text" name="fname"  class="form-control" placeholder="First Name"></td>
+                </tr>
+                 <tr>
+                   <td>Last Name</td>
+                   <td><input type="text" name="lname"  class="form-control" placeholder="Last Name"></td>
+                </tr>
+                  <tr>
+                   <td>Contact Number</td>
+                   <td><input type="text" name="lname"  class="form-control" placeholder="0711234567"></td>
+                </tr>
+                <tr>
+                   <td>Email Address</td>
+                   <td><input type="email" name="email"  class="form-control" placeholder="email Address"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" name="pwd1"  class="form-control" placeholder="password"> </td>
+                </tr>
+                 <tr>
+                    <td>Confirm Password</td>
+                    <td><input type="password" name="pwd2"  class="form-control" placeholder="Confirm password"> </td>
+                </tr>
+                
+                 <tr>
+                     <td></td>
+                     <td>
+                        <input type="submit" name="submit" value="Register" class="btn">
+                        <input type="reset" value="Cansel" class="btn">
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Do you have an account? <a href="#" id="log">Log in</a> </td>
+                </tr>
+            </table>
+        </form>
+        <!---->
     </div>
     
-    
+    <script>
+        $(document).ready(function(){
+           $("#sing_in").click(function(){
+            $("#log_tb").fadeOut("3000");
+            $("#rge_tb").fadeIn("3000");
+            });
+            
+             $("#log").click(function(){
+                $("#rge_tb").fadeOut("3000");
+                 $("#log_tb").fadeIn("3000");
+             });
+        });
+    </script>
 </body>
 </html>
